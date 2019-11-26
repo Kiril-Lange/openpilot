3b77c7670b8455a98f26076be0c6d04bcaab7d00
theantihero <kwj129@gmail.com>
2019-11-26 15:08:48 -0500

    update: kegman_conf.py

--- selfdrive/kegman_conf.py
+++ selfdrive/kegman_conf.py
@@ -132,7 +132,7 @@ class kegman_conf():
         self.element_updated = True
 
       if "simpledd" not in self.config:
-        self.config.update({"simpledd":False})
+        self.config.update({"simpledd":"0"})
         self.element_updated = True
 
       if self.element_updated:
@@ -148,7 +148,7 @@ class kegman_conf():
 		     "3barBP1":"3.0", "1barMax":"2.1", "2barMax":"2.1", "3barMax":"2.1", \
 		     "1barHwy":"0.4", "2barHwy":"0.3", "3barHwy":"0.1", \
          "steerRatio":"-1", "steerRateCost":"-1", "slowOnCurves":"0", "Kf":"-1", "lane_hug_direction":"None", "lane_hug_mod":"1.2", "lane_hug_angle":"10", "zorro_mod":"1", "shane_mod":"1", \
-                     "sR_boost": "4.5", "sR_BP0": "3.5", "sR_BP1": "10", "sR_time": "1.5", "simpledd":False}
+                     "sR_boost": "4.5", "sR_BP0": "3.5", "sR_BP1": "10", "sR_time": "1.5", "simpledd":"0"}
 
       self.write_config(self.config)
     return self.config
