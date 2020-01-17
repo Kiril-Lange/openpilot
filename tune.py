9035f1fed7b9f8a1fef40a2f8a3259385cb08b22
Comma Device <device@comma.ai>
2020-01-17 10:37:13 -0500

    update

--- tune.py
+++ tune.py
@@ -41,7 +41,7 @@ button_delay = 0.2
 kegman = kegman_conf()
 kegman.conf['tuneGernby'] = "1"
 #kegman.write_config(kegman.conf)
-param = ["Kp", "Ki", "Kf", "steerRatio", "steerRateCost", "sR_boost", "sR_BP0", "sR_BP1", "sR_time", "steerRateCost", "deadzone"]#, \
+param = ["Kp", "Ki", "Kf", "cameraOffset", "steerRatio", "steerRateCost", "sR_boost", "sR_BP0", "sR_BP1", "sR_time", "steerRateCost", "deadzone"]#, \
          #"1barBP0", "1barBP1", "1barMax", "2barBP0", "2barBP1", \
          #"2barMax", "3barBP0", "3barBP1", "3barMax", \
          #"1barHwy", "2barHwy", "3barHwy"]
