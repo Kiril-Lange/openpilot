b59b1e30e6e1d751e80b272412ac1288df8d59a0
Comma Device <device@comma.ai>
2020-02-18 02:09:35 -0500

    update tune

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -364,8 +364,9 @@ class CarInterface(CarInterfaceBase):
       # 3600 is 3x max of 1200
       # command of 0xF00 * 0xDDB4 >> 0xF >> 0x2 = 0x67E torque table index -> 3600 torque table output
       # torque table goes up to 0x6EE but lookup function input is clamped at 67E therefore max request is 0xF00
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0x0, 0x500, 0x800, 0xA80, 0xCE6, 0x1000, 0x1180, 0x1200, 0x2600], [0x0, 0x100, 0x200, 0x300, 0x400, 0x600, 0x800, 0xA00, 0xF00]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.4], [0.12]]
+      ret.lateralParams.torqueBP = [0, 2560, 8000]
+      ret.lateralParams.torqueV  = [0, 2560, 3840]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.1]]
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
       ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
       ret.longitudinalTuning.kiBP = [0., 35.]
