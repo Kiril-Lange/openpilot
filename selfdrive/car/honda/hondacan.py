62b6f7a226aa769171ff60d64fb9f03dd8df1ba4
Comma Device <device@comma.ai>
2020-04-16 08:34:23 -0400

    fix

--- selfdrive/car/honda/hondacan.py
+++ selfdrive/car/honda/hondacan.py
@@ -75,7 +75,7 @@ def create_steering_control(packer, apply_steer, lkas_active, car_fingerprint, i
     "STEER_TORQUE": apply_steer if lkas_active else 0,
     "STEER_TORQUE_REQUEST": lkas_active,
   }
-  bus = get_lkas_cmd_bus(car_fingerprint, has_relay)
+  bus = get_lkas_cmd_bus(car_fingerprint, has_relay, openpilot_longitudinal_control)
   return packer.make_can_msg("STEERING_CONTROL", bus, values, idx)
 
 def create_steering_control_x2(packer, car_fingerprint, idx, has_relay):
@@ -85,7 +85,7 @@ def create_steering_control_x2(packer, car_fingerprint, idx, has_relay):
     "BYTE_2": 0x80,
     "BYTE_3": 0x10,
   }
-  bus = get_lkas_cmd_bus(car_fingerprint, has_relay)
+  bus = get_lkas_cmd_bus(car_fingerprint, has_relay, openpilot_longitudinal_control)
   return packer.make_can_msg("STEERING_CONTROL_X2", bus, values, idx)
 
 def create_ui_commands(packer, pcm_speed, hud, car_fingerprint, is_metric, idx, has_relay, openpilot_longitudinal_control, stock_hud):
