5ec024d512686ca51639895afad2292a3b8f6058
theantihero <kwj129@gmail.com>
2020-04-17 22:44:43 -0400

    Update interface.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -379,11 +379,11 @@ class CarInterface(CarInterfaceBase):
       if eps_modified:
         ret.lateralParams.torqueBP = [0, 1536, 2027, 2662, 2969, 3071, 7167]
         ret.lateralParams.torqueV  = [0, 1536, 2048, 2560, 3072, 3584, 4096]
-        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.42], [0.16]]
+        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.4], [0.12]]
         ret.lateralTuning.pid.kf = 0.00004
         ret.steerRateCost = 0.35
       else:
-        ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 3840], [0, 3840]] # TODO: determine if there is a dead zone at the top end
+        ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]] # TODO: determine if there is a dead zone at the top end
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.8], [0.24]]
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
       ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
@@ -410,14 +410,14 @@ class CarInterface(CarInterfaceBase):
     ret.tireStiffnessFront, ret.tireStiffnessRear = scale_tire_stiffness(ret.mass, ret.wheelbase, ret.centerToFront,
                                                                          tire_stiffness_factor=tire_stiffness_factor)
     if candidate in HONDA_BOSCH:
-      #ret.gasMaxBP = [0.]   # m/s
-      #ret.gasMaxV = [0.5]
       ret.gasMaxBP = [0.0, 1.4082, 2.80311, 4.22661, 5.38271, 6.16561, 7.24781, 8.28308, 10.24465, 12.96402, 15.42303, 18.11903, 20.11703, 24.46614, 29.05805, 32.71015, 35.76326]  # m/s
-      ret.gasMaxV = [0.39, 0.43, 0.425, 0.42, 0.415, 0.4, 0.4832, 0.375, 0.38, 0.383, 0.385, 0.388, 0.39, 0.4, 0.41, 0.42, 0.43] # percentage of gas
-      ret.brakeMaxBP = [0., 20., 25., 30.]  # m/s
-      ret.brakeMaxV = [1., 0.8, 0.6, 0.5]   # max brake allowed
-      #ret.brakeMaxBP = [0.] # m/s
-      #ret.brakeMaxV = [1.]  # max brake allowed
+      ret.gasMaxV = [0.69, 0.73, 0.725, 0.72, 0.715, 0.7, 0.6832, 0.675, 0.68, 0.683, 0.685, 0.688, 0.69, 0.7, 0.71, 0.72, 0.73] # percentage of gas
+      ret.brakeMaxBP = [0., 20.]  # m/s
+      ret.brakeMaxV = [1., 0.8]   # max brake allowed
+      #ret.gasMaxBP = [0.]   # m/s
+      #ret.gasMaxV = [0.6]
+      #ret.brakeMaxBP = [0., 5., 20.]  # m/s
+      #ret.brakeMaxV = [1., 1., 0.8]   # max brake allowed
     else:
       ret.gasMaxBP = [0.]  # m/s
       ret.gasMaxV = [0.6] if ret.enableGasInterceptor else [0.] # max gas allowed
